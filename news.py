3079522764fe339d49fc34634de0e2c3b8e5b58b
santosh vijapure <vijaupuresantosh@gmail.com>
2020-05-16 10:33:49 +0530

    tokernizer refactor in seperate module

--- news.py
+++ news.py
@@ -3,24 +3,12 @@ import json
 from keras.models import load_model
 import tensorflow_hub as hub
 import numpy as np
-
+import tokenizer
 tf_model = load_model('./keras_model.h5')
-
-module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
-
-model = hub.load(module_url)
-
-print ("module %s loaded" % module_url)
-
-# to create embedings
-
-def embed(input):
-  return model(input)
-
 
 def my_function(news):
     print(news)
-    encoded_vec=embed(news).numpy()
+    encoded_vec=tokenizer.embed(news).numpy()
     result=tf_model.predict(encoded_vec)[0][0]
     if result > 0.5 :
         return 1
